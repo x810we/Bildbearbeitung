027f93104c0b668d75edffd51eade76bcda55153
x810we <ingo.weyck@t-online.de>
2023-06-05 17:35:44 +0200

    initial Commit No 1393

--- src/Test/BlueImage.java
+++ src/Test/BlueImage.java
@@ -38,13 +38,10 @@ public class BlueImage{
         for(int y = 0; y < height; y++){
             for(int x = 0; x < width; x++){
                 int p = img.getRGB(x,y);
-
                 int a = (p>>24)&0xff;
                 int b = p&0xff;
-
                 //set new RGB
                 p = (a<<24) | (0<<16) | (0<<8) | b;
-
                 img.setRGB(x, y, p);
             }
         }
